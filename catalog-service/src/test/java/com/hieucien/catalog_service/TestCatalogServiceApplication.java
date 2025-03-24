15cbc9ea9265381f2a79f96099bdd3e00899d0aa
congnghiahieu <hieucn.work@gmail.com>
2025-03-24 17:26:12 +0700

    init project

---
+++ catalog-service/src/test/java/com/hieucien/catalog_service/TestCatalogServiceApplication.java
@@ -0,0 +1,11 @@
+package com.hieucien.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
